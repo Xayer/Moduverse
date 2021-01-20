bd52194080855b10d067b2b7c2837981fb299c2f
Frederik Rabøl <frederik-rm@hotmail.com>
2021-01-20 20:02:38 +0100

    added dev property, just to make sure

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,7 @@ export default {
       "src": "https://cdn.jsdelivr.net/npm/focus-visible@5.0.2/dist/focus-visible.min.js"
     }]
   },
+  dev: process.env.NODE_ENV !== 'production',
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ["vue-essential-slices/src/styles/styles.scss", "@/assets/scss/main.scss"],
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
